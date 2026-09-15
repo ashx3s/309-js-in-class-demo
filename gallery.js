d3e64283589aed749557896b80f4fa2a347fa290
Ashlyn Knox <ashx3s@tuta.io>
2026-09-15 15:14:45 -0600

    added static image example

---
+++ gallery.js
@@ -0,0 +1,16 @@
+const galleryImages = [
+  {
+    imageSrc: "images/bike-river-meditation.jpg",
+    altTag:
+      "Someone sitting against a tree by a river with their bike. probably meditating.",
+  },
+  {
+    imageSrc: "images/bird-over-water",
+    altTag:
+      "a bird flying over a body of water with an island in the background.",
+  },
+  {
+    imageSrc: "images/ferris-wheel.jpg",
+    altTag: "A ferris wheel at a fair or something.",
+  },
+];
